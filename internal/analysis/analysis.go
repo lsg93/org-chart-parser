98c637c3dfa8a9106a7a0373526af8e9d5321799
lsg93 <l-s.gwam@gmx.com>
2025-11-13 01:44:06 +0000

    Tests + ensuring unique names dont break things

--- internal/analysis/analysis.go
+++ internal/analysis/analysis.go
@@ -1,36 +1,102 @@
 package analysis
 
 import (
+	"errors"
 	"fmt"
 	"io"
+	"sort"
 	"strings"
 
 	"github.com/lsg93/org-chart-parser/internal/model"
 )
 
+var (
+	errAnalysisNoPathsFound          = errors.New("No suitable paths between the given employees could be found.")
+	errAnalysisInvalidNameArgument   = errors.New("One, or both of the names provided as arguments do not exist in the organisation chart.")
+	errAnalysisDuplicateNameArgument = errors.New("Tthe names provided as arguments are duplicate - there is no way to determine the path you would like to see.")
+)
+
 type organisationChartAnalyser struct {
-	chart  model.OrganisationChart
-	output io.Writer
+	chart   model.OrganisationChart
+	output  io.Writer
+	adjList map[int][]int    // graph structure for BFS traversal
+	nameMap map[string][]int // used to look up names when building string from path ID's
 }
 
 type OrganisationChartAnalysis struct{}
 
 func NewOrganisationChartAnalyser(output io.Writer, chart model.OrganisationChart) *organisationChartAnalyser {
-	return &organisationChartAnalyser{chart: chart, output: output}
+	analyser := &organisationChartAnalyser{
+		chart:  chart,
+		output: output,
+	}
+
+	analyser.adjList = analyser.mapEmployees()
+	analyser.nameMap = analyser.mapEmployeeNames()
+
+	return analyser
 }
 
 // Breadth-first search to traverse graph.
 // If we wanted to make this code as optimal as possi
 func (a *organisationChartAnalyser) Analyse(name1 string, name2 string) error {
 
-	// Need to validate here before any operations.
+	// Validate that the names actually exist
+	err := a.validateNames(name1, name2)
 
-	adjList := a.mapEmployees()
-	nameMap := a.mapEmployeeNames()
+	if err != nil {
+		return err
+	}
 
-	startId := nameMap[name1]
-	targetId := nameMap[name2]
+	/*
+		There is no guarantee names are unique
+		As such, the only thing we can really do to calculate the shortest path
+		Is generate all possible paths for each instance of a duplicated name
+		And use the shortest one in our final output
+	*/
 
+	startIds := a.nameMap[name1]
+	targetIds := a.nameMap[name2]
+
+	// Store all the paths so we can then sort them to find and return the shortest one
+	allPaths := make([][]int, 0)
+
+	for _, startId := range startIds {
+		for _, targetId := range targetIds {
+			pathIds := a.search(startId, targetId)
+			path, err := a.constructPath(startId, targetId, pathIds)
+
+			if err != nil {
+				return errAnalysisNoPathsFound
+			}
+
+			allPaths = append(allPaths, path)
+		}
+	}
+
+	// Sort all of our calculated paths, push the shortest one to the front.
+	sort.Slice(allPaths, func(i int, j int) bool {
+		return len(allPaths[i]) < len(allPaths[j])
+	})
+
+	// The shortest path gets turned into a string.
+	shortestPath, err := a.pathToString(allPaths[0])
+
+	if err != nil {
+		return err
+	}
+
+	_, err = a.output.Write([]byte(shortestPath.String()))
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// BFS algorithm.
+func (a *organisationChartAnalyser) search(startId int, targetId int) map[int]int {
 	queue := []int{startId}
 
 	// Use a map for quicker lookup of seenIds.
@@ -42,14 +108,16 @@ func (a *organisationChartAnalyser) Analyse(name1 string, name2 string) error {
 		currentId := queue[0]
 		queue = queue[1:] // Shift current item off start of queue.
 
-		for _, relationId := range adjList[currentId] {
+		for _, relationId := range a.adjList[currentId] {
+			// If the next node hasn't been seen, then add it to the queue.
+			// All nodes at a particular depth get added to the queue and get 'seen'.
 			if !seenIds[relationId] {
 				seenIds[relationId] = true
 				pathIds[relationId] = currentId
 				queue = append(queue, relationId)
 
 				if relationId == targetId {
-					return a.constructPath(startId, targetId, pathIds)
+					return pathIds
 				}
 			}
 
@@ -57,10 +125,10 @@ func (a *organisationChartAnalyser) Analyse(name1 string, name2 string) error {
 
 	}
 
-	return a.constructPath(startId, targetId, pathIds)
+	return pathIds
 }
 
-func (a *organisationChartAnalyser) constructPath(startId int, targetId int, pathMap map[int]int) error {
+func (a *organisationChartAnalyser) constructPath(startId int, targetId int, pathMap map[int]int) ([]int, error) {
 
 	// Iterate through map starting from targetId to build path.
 	path := make([]int, 0)
@@ -68,16 +136,28 @@ func (a *organisationChartAnalyser) constructPath(startId int, targetId int, pat
 	currentId := targetId
 
 	for currentId != startId {
+
+		prev, ok := pathMap[currentId]
+
+		if !ok {
+			// Can't find the id in the map - path is invalid
+			return []int{}, errAnalysisNoPathsFound
+		}
+
 		path = append([]int{currentId}, path...)
-		currentId = pathMap[currentId]
+
+		currentId = prev
 	}
 
 	path = append([]int{startId}, path...)
 
-	// Now that we have the shortest path, we have to analyse the direction of the data flow.
-	// We iterate through the path, and determine whether the direction of travel is up/down based on whether the next item in the slice has a manager.
-	// I think you could use a bi-directional BFS for this in future for better performance maybe if it was critical.
+	return path, nil
+}
 
+// // In order to achieve the desired output, we have to analyse the direction of the data flow.
+// // We iterate through the path, and determine whether the direction of travel is up/down based on whether the next item in the slice has a manager.
+// // I think you could use a bi-directional BFS for this in future for better performance maybe if it was critical.
+func (a *organisationChartAnalyser) pathToString(path []int) (strings.Builder, error) {
 	idMap := a.mapEmployeeIds()
 	managerMap := a.mapManagement()
 
@@ -94,18 +174,33 @@ func (a *organisationChartAnalyser) constructPath(startId int, targetId int, pat
 			flowDirection = "<-"
 		}
 
-		stringsPath.WriteString(fmt.Sprintf("%s %s ", idMap[current], flowDirection))
+		_, err := stringsPath.WriteString(fmt.Sprintf("%s %s ", idMap[current], flowDirection))
+
+		if err != nil {
+			return stringsPath, err
+		}
 	}
 
 	// Manually append last item in path, since we won't iterate over it.
-	stringsPath.WriteString(idMap[path[len(path)-1]])
-
-	fmt.Println(stringsPath.String())
-
-	_, err := a.output.Write([]byte(stringsPath.String()))
+	_, err := stringsPath.WriteString(idMap[path[len(path)-1]])
 
 	if err != nil {
-		return nil
+		return stringsPath, err
+	}
+
+	return stringsPath, nil
+}
+
+func (a *organisationChartAnalyser) validateNames(name1 string, name2 string) error {
+	if name1 == name2 {
+		return errAnalysisDuplicateNameArgument
+	}
+
+	_, startExists := a.nameMap[name1]
+	_, endExists := a.nameMap[name2]
+
+	if !startExists || !endExists {
+		return errAnalysisInvalidNameArgument
 	}
 
 	return nil
@@ -142,18 +237,19 @@ func (a *organisationChartAnalyser) mapManagement() map[int]int {
 
 // Create easy lookups to translate ID's to names.
 func (a *organisationChartAnalyser) mapEmployeeIds() map[int]string {
-	nameMap := make(map[int]string)
+	idMap := make(map[int]string)
 	for _, employee := range a.chart {
-		nameMap[employee.Id] = fmt.Sprintf("%s (%d)", employee.Name, employee.Id)
+		idMap[employee.Id] = fmt.Sprintf("%s (%d)", employee.Name, employee.Id)
 	}
-	return nameMap
+	return idMap
 }
 
-// Create easy lookups to translate names to ID's.
-func (a *organisationChartAnalyser) mapEmployeeNames() map[string]int {
-	nameMap := make(map[string]int)
+// Create easy lookups to translate names to ID's - we need to have slices instead of a hashmap
+// Because names might not be unique.
+func (a *organisationChartAnalyser) mapEmployeeNames() map[string][]int {
+	nameMap := make(map[string][]int)
 	for _, employee := range a.chart {
-		nameMap[employee.Name] = employee.Id
+		nameMap[employee.Name] = append(nameMap[employee.Name], employee.Id)
 	}
 	return nameMap
 }
--- internal/analysis/analysis_test.go
+++ internal/analysis/analysis_test.go
@@ -17,7 +17,7 @@ var exampleOrgChart = model.OrganisationChart{
 	model.Employee{Id: 17, Name: "Catwoman", ManagerId: 6},
 }
 
-func setupTestAnalyser(chart model.OrganisationChart, t *testing.T) (*organisationChartAnalyser, *testWriter) {
+func setupTestAnalyser(chart model.OrganisationChart) (*organisationChartAnalyser, *testWriter) {
 	// Output needs to go to a writer to make assertions against.
 	tw := &testWriter{}
 	return NewOrganisationChartAnalyser(tw, chart), tw
@@ -54,25 +54,28 @@ func TestAnalysisReturnsShortestPathAsString(t *testing.T) {
 			employee2:      "Catwoman",
 			expectedOutput: "Batman (16) -> Black Widow (6) <- Catwoman (17)",
 		},
-		// "handles duplicates": {
-		// 	input: model.OrganisationChart{
-		// 		model.Employee{Id: 1, Name: "Minion", ManagerId: 1},
-		// 		model.Employee{Id: 2, Name: "Minion", ManagerId: 1},
-		// 	},
-		// 	employee1:      "Minion",
-		// 	employee2:      "Minion",
-		// 	expectedOutput: "Minion (1) -> Minion (2)",
-		// },
+		"handles duplicates gracefully": {
+			input: model.OrganisationChart{
+				model.Employee{Id: 1, Name: "CEO", ManagerId: 0},
+				model.Employee{Id: 2, Name: "Boss", ManagerId: 1},
+				model.Employee{Id: 10, Name: "Minion", ManagerId: 1}, // This should be the one referenced.
+				model.Employee{Id: 3, Name: "Boss", ManagerId: 0},
+				model.Employee{Id: 20, Name: "Minion", ManagerId: 2}, // This path would have 2 jumps.
+			},
+			employee1:      "Minion",
+			employee2:      "CEO",
+			expectedOutput: "Minion (10) -> CEO (1)",
+		},
 	}
 
 	for desc, tc := range testCases {
 		t.Run(desc, func(t *testing.T) {
-			analyser, writer := setupTestAnalyser(tc.input, t)
+			analyser, writer := setupTestAnalyser(tc.input)
 			err := analyser.Analyse(tc.employee1, tc.employee2)
 			output := writer.contents
 
 			if err != nil {
-				t.Fatalf("There was an error analysing the given input.")
+				t.Fatalf("There was an error '%s' analysing the given input.", err.Error())
 			}
 
 			if output != tc.expectedOutput {
@@ -83,28 +86,54 @@ func TestAnalysisReturnsShortestPathAsString(t *testing.T) {
 
 }
 
-// func TestAnalysisHandlesDuplicateNames() {
-// 	// With contiguous whitespace
-// 	// With non-contiguous whitespace
-// 	// With different casing
-// }
+func TestAnalysisErrorsWithInvalidArguments(t *testing.T) {
+	type testCase struct {
+		input         model.OrganisationChart
+		employee1     string
+		employee2     string
+		expectedError error
+	}
 
-// func testAnalysisErrorsIfDuplicateIdsArePresent(t *testing.T) {
+	testCases := map[string]testCase{
+		"Non-existent name as argument": {
+			input:         exampleOrgChart,
+			employee1:     "Superman",
+			employee2:     "Batman",
+			expectedError: errAnalysisInvalidNameArgument,
+		},
+		"Two duplicate names as arguments": {
+			input:         exampleOrgChart,
+			employee1:     "Batman",
+			employee2:     "Batman",
+			expectedError: errAnalysisDuplicateNameArgument,
+		},
+		"When an invalid path is attempted": {
+			input: model.OrganisationChart{
+				model.Employee{Id: 1, Name: "CEO", ManagerId: 0},
+				model.Employee{Id: 2, Name: "VP", ManagerId: 1},
+				model.Employee{Id: 3, Name: "CTO", ManagerId: 0},
+				model.Employee{Id: 4, Name: "SWE", ManagerId: 3},
+			},
+			employee1:     "SWE",
+			employee2:     "VP",
+			expectedError: errAnalysisNoPathsFound,
+		},
+	}
 
-// }
+	for desc, tc := range testCases {
+		t.Run(desc, func(t *testing.T) {
+			analyser, writer := setupTestAnalyser(tc.input)
+			err := analyser.Analyse(tc.employee1, tc.employee2)
+			output := writer.contents
 
-// func testAnalysisHandlesEmptyOrganisationChartGracefully(t *testing.T) {
+			if err == nil {
+				t.Fatalf("There was no error analysing the given input when one should have occurred.")
+			}
 
-// }
+			if err != tc.expectedError {
+				t.Errorf("The received error '%s' was not equal to the expected output '%s'", output, tc.expectedError)
+			}
+		})
+	}
 
-// func testAnalysisErrorsIfInputIsNonExistentNameisSupplied(t *testing.T) {
-
-// }
-
-// func testAnalysisHandlesNonExistentIds(t *testing.T) {
-
-// }
-
-// func TestAnalysisErrorsWhenCyclicalPathFound() (t *testing.T) {
-
-// }
+}
